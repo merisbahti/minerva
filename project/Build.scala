04c478d3e3dd86dd0830d53c0dca6d2683205c88
Meris <meris.bahti@gmail.com>
2014-11-06 11:47:39 +0100

    fix deps

---
+++ project/Build.scala
@@ -0,0 +1,17 @@
+import sbt._
+import Keys._
+
+object ProjectBuild extends Build {
+
+  val projectSettings = Defaults.defaultSettings ++ Seq(
+      name := "edan70-unknown",
+      version := "1.0",
+      organization := "se.lth.cs",
+      scalaVersion := "2.11.2",
+      libraryDependencies += "org.apache.lucene" % "lucene-core" % "4.10.2"
+  )
+
+  val myProject = Project("edan70-unknown", file("."), settings = projectSettings)
+
+}
+

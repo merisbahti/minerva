59cd5a4eb87601e28f46f7e458b8a587e24494c5
Meris <meris.bahti@gmail.com>
2014-11-07 18:53:11 +0100

    Don't add stub, redirect, special or "disambigious" pages. Also, PLEASE USE 4 (or 2, even 8) SPACES FOR INDENTATION!

--- src/main/java/jules/Indexer.java
+++ src/main/java/jules/Indexer.java
@@ -14,45 +14,48 @@ import edu.jhu.nlp.wikipedia.*;
 
 
 public class Indexer {
-	public static void Main(String[] args){
-		Indexer indexer = new Indexer();
-		indexer.index();
-	}
-	private String wikiFile;
-	private String indexDir;
-	
-	
-	public Indexer(){
-		wikiFile = "./svwiki-20141031-pages-meta-current.xml";
-		indexDir = "./indexDir/";
-	}
-	
-	public void index(){
-		//WikiXMLParser wxp = WikiXMLParserFactory.getDOMParser(wikiFile);
-		Analyzer analyzer = new StandardAnalyzer();
-		IndexWriterConfig iwc = new IndexWriterConfig(Version.LATEST, analyzer);
-		try{
-			final IndexWriter writer = new IndexWriter(FSDirectory.open(new File(indexDir)), iwc);
-			WikiXMLParser wxsp = WikiXMLParserFactory.getSAXParser(wikiFile);
-			wxsp.setPageCallback(new PageCallbackHandler() {
-				@Override
-				public void process(WikiPage page) {
-					Document doc = new Document();
-					System.out.println(page.getID() + ", title: " + page.getTitle());
-					doc.add(new IntField("id", Integer.parseInt(page.getID()), Field.Store.YES));
-					doc.add(new TextField("title", page.getTitle(), Field.Store.YES));
-					doc.add(new TextField("text", page.getText(), Field.Store.YES));
-					try {
-						writer.addDocument(doc);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					
-				}
-			});
-			wxsp.parse();
-			writer.commit();
-			writer.close();
-		}catch(Exception e){ e.printStackTrace(); }
-	}
+    public static void Main(String[] args){
+        Indexer indexer = new Indexer();
+        indexer.index();
+    }
+    private String wikiFile;
+    private String indexDir;
+
+
+    public Indexer(){
+        wikiFile = "./svwiki-20141031-pages-meta-current.xml";
+        indexDir = "./indexDir/";
+    }
+
+    public void index(){
+        //WikiXMLParser wxp = WikiXMLParserFactory.getDOMParser(wikiFile);
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(Version.LATEST, analyzer);
+        try{
+            final IndexWriter writer = new IndexWriter(FSDirectory.open(new File(indexDir)), iwc);
+            WikiXMLParser wxsp = WikiXMLParserFactory.getSAXParser(wikiFile);
+            wxsp.setPageCallback(new PageCallbackHandler() {
+                @Override
+                public void process(WikiPage page) {
+                    // We don't want to store unnecessary pages.
+                    if (page.isDisambiguationPage() || page.isRedirect() || page.isSpecialPage() || page.isStub())
+                        return;
+                    Document doc = new Document();
+                    System.out.println(page.getID() + ", title: " + page.getTitle());
+                    doc.add(new IntField("id", Integer.parseInt(page.getID()), Field.Store.YES));
+                    doc.add(new TextField("title", page.getTitle(), Field.Store.YES));
+                    doc.add(new TextField("text", page.getText(), Field.Store.YES));
+                    try {
+                        writer.addDocument(doc);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            wxsp.parse();
+            writer.commit();
+            writer.close();
+        }catch(Exception e){ e.printStackTrace(); }
+    }
 }

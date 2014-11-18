8f4a312fe6f0c9827cdc63f1ffbbab58e7a0397d
Meris <meris.bahti@gmail.com>
2014-11-18 09:41:00 +0100

    stop taking context, we got good server now.

--- src/main/java/jules/WebService.java
+++ src/main/java/jules/WebService.java
@@ -16,7 +16,7 @@ import com.sun.net.httpserver.HttpServer;
 
 public class WebService {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
         server.createContext("/", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -26,9 +26,7 @@ public class WebService {
         public void handle(HttpExchange t) throws IOException {
             String response = "No query found, specify the query parameter.";
             Map <String,String> qMap = queryToMap(t.getRequestURI().getQuery() == null ? "" : t.getRequestURI().getQuery());
-            System.out.println("checking for key");
             if (qMap.containsKey("q")) {
-                System.out.println("key found");
                 StringBuilder sb = new StringBuilder();
                 String q = qMap.get("q");
                 List<Map<String, String>> results =  jules.Indexer.query(q, true);
@@ -40,7 +38,8 @@ public class WebService {
                         sb.append(entry.getKey() + ": \n");
                         if (entry.getKey().equals("text")) {
                             int indexOfHit = entry.getValue().toLowerCase().indexOf(q);
-                            String context = entry.getValue().toLowerCase().subSequence(indexOfHit-20, indexOfHit+20).toString()+"\n";
+                            //String context = entry.getValue().toLowerCase().subSequence(indexOfHit-20, indexOfHit+20).toString()+"\n";
+                            String context = entry.getValue();
                             sb.append(context);
                             res.addProperty(entry.getKey(), entry.getValue());
                         } else {

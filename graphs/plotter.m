b332cdb293999bf5609c649f989be217d3557452
Meris <meris.bahti@gmail.com>
2014-11-20 14:57:38 +0100

    add the new data to the plot-generating-code

--- graphs/plotter.m
+++ graphs/plotter.m
@@ -3,15 +3,19 @@ function plotter()
   % Scan the data from the incomplete data-set.
   [xbm25,ybm25] = scan_data("bm25.txt");
   [xtfdf,ytfdf] = scan_data("tfdf.txt");
+  [xbm25doc,ybm25doc] = scan_data("bm25-doc.txt");
+  [xtfdfdoc,ytfdfdoc] = scan_data("tfdf-doc.txt");
   %rgb(230, 126, 34)
   plot(xbm25,ybm25,'marker','o','color', [230/255 126/255 34/255]);
   hold on;
   %rgb(52, 152, 219)
   plot(xtfdf,ytfdf,'marker','o','color', [52/255 152/255 219/255]);
+  plot(xbm25doc,ybm25doc,'marker','o','color', [211/255 84/255 0/255]);
+  plot(xtfdfdoc,ytfdfdoc,'marker','o','color', [41/255 128/255 185/255]);
   xlim([1 350])
 
-  legend('BM25', 'TFDF');
-  xlabel('#Paragraphs')
+  legend('BM25 Paragraphs', 'TFDF Paragraphs', 'BM25 Documents', 'TFDF Documents');
+  xlabel('#Passages')
   ylabel('Answers present')
   a=[cellstr(num2str(get(gca,'ytick')'*100))]; 
   pct = char(ones(size(a,1),1)*'%'); 

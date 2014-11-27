d0f7aa9833cc8bff9046c2813a438d561071fe73
Dolck <tim.dolck@gmail.com>
2014-11-27 13:39:54 +0100

    more test on ranknouns

--- src/main/java/test/RankNounsTest.java
+++ src/main/java/test/RankNounsTest.java
@@ -12,7 +12,8 @@ public class RankNounsTest {
 
 	@Test
 	public void test() {
-		List<Map<String,String>> res = QueryPassenger.query("Stockholm", 10);
+		List<Map<String,String>> res = QueryPassenger.query("Vad heter Sveriges huvudstad", 100);
+		
 		LinkedHashMap<String, Integer> lm = QueryPassenger.findTopNouns(res);
 		//Display top 30 frequent nouns
 		int i = 0;
@@ -20,6 +21,17 @@ public class RankNounsTest {
 			if(i > 30)
 				break;
 			System.out.println(s + " \t" + lm.get(s));
+			i++;
+		}
+		
+		res = QueryPassenger.query("Vad heter Islands huvudstad", 100);
+		lm = QueryPassenger.findTopNouns(res);
+		i = 0;
+		for(String s : lm.keySet()){
+			if(i > 100)
+				break;
+			System.out.println(s + " \t" + lm.get(s));
+			i++;
 		}
 	}
 

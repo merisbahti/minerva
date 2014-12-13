e4373ec63df7ffccb028aa4143525a395289967c
Dolck <tim.dolck@gmail.com>
2014-12-13 13:58:20 +0100

    constants

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,8 +3,9 @@ package util;
 public class Constants {
 	public static final String qDir = "./questions/";
 	public static final String indexDir = "./indexDir/";
+	public static final String modelsDir = "./model/";
 	public static final String questions = "./questions";
-	public static final String indexFile = "./model/training_indexes.txt";
+	public static final String indexFile = modelsDir + "training_indexes.txt";
 	public static String wikiFile = "./sewiki-20141104-pages-meta-current.xml";
 	public static String outputDir = "./output/";
 

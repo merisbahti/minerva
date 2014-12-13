9aec9acda7afe5cf2e4cda915af3784cb0a9f95f
Jullekungfu <julian.krone@hotmail.com>
2014-12-13 13:57:45 +0100

    add final

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String indexDir = "./indexDir/";
 	public static final String questions = "./questions";
 	public static final String indexFile = "./model/training_indexes.txt";
-	public static String wikiFile = "./sewiki-20141104-pages-meta-current.xml";
-	public static String outputDir = "./output/";
+	public static final String wikiFile = "./sewiki-20141104-pages-meta-current.xml";
+	public static final String outputDir = "./output/";
 
 }

75dff32693950ddffae2ad8216e083aa38582679
Dolck <tim.dolck@gmail.com>
2014-12-14 12:46:40 +0100

    catch em exceptions

--- src/main/java/test/TestRerank.java
+++ src/main/java/test/TestRerank.java
@@ -3,20 +3,21 @@ package test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 
 import jules.Categorizer;
 import jules.QueryPassager;
 import jules.RankNouns;
 import jules.Reranker;
 import jules.ScoreWord;
-
 import tagging.PosTagger;
 import tagging.Word;
 import util.Constants;
@@ -31,7 +32,11 @@ public class TestRerank {
 	public static void setUp() throws Exception {
 		questions = new HashMap<String, String>();
 		File dir = new File(Constants.qDir);
-		writer = new PrintWriter("test.txt", "UTF-8");
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String outFile = queries + "P:" + sdf.format(date) + ".txt";
+		System.out.println("OutFile: " + outFile);
+		writer = new PrintWriter(outFile, "UTF-8");
 		for (File f : dir.listFiles()) {
 			if(f.getName().startsWith(".")) continue;
 			System.out.println("Reading file: " + f.getName());
@@ -53,7 +58,12 @@ public class TestRerank {
 		for (Entry<String, String> question : questions.entrySet()) {
 			List<Map<String, String>> list = QueryPassager.query(question.getKey(), queries);
 			List<ScoreWord> topN = RankNouns.findTopNouns(list);
-			List<Pair<String, Double>> cat = Categorizer.getCategories(question.getKey());
+			List<Pair<String, Double>> cat;
+			try{
+				cat = Categorizer.getCategories(question.getKey());
+			}catch(NoSuchElementException e){
+				continue;
+			}
 			Reranker ins = Reranker.getInstance();
 			PosTagger tagger = PosTagger.getInstance();
 			List<Word[]> words = tagger.tagString(question.getKey());
@@ -68,7 +78,7 @@ public class TestRerank {
 				i++;
 				if (s.equalsIgnoreCase(question.getValue())){
 					//			  index found 		rank				total number of nouns
-					writer.println(i + "\t" + sw.getTotalRank() + "\t" + results.size());
+					writer.println(i + "\t" + sw.getTotalRank() + "\t" + results.size() + "\t" + question.getKey());
 					writer.flush();
 					break;
 				}

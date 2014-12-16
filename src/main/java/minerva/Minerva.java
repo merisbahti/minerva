6878757ae2bb272ab78efa96645d4d15700de40e
meris <meris.bahti@gmail.com>
2014-12-16 14:45:28 +0100

    fix minerva

--- src/main/java/minerva/Minerva.java
+++ src/main/java/minerva/Minerva.java
@@ -16,28 +16,28 @@ import jules.Reranker;
 import jules.ScoreWord;
 
 public class Minerva {
-	private static List<Map<String,String>> lastQuery;
-	private static List<ScoreWord> lastTopNouns;
-	private static List<ScoreWord> topNouns;
-	private static String q;
-	private static List<ScoreWord> topRerankedNouns;
+	private List<Map<String,String>> lastQuery;
+	private List<ScoreWord> topNouns;
+	private List<ScoreWord> topRerankedNouns;
+	private String q;
 
 	public Minerva(String query) {
 		q = Constants.whiteList(query);
-		lastTopNouns = null;
-		topNouns = null;
-		topRerankedNouns = null;
 		lastQuery = QueryPassager.query(q, 100);
 	}
 
-	public List<ScoreWord> findTopNouns(){
-		if (lastTopNouns == null) {
+	public List<Map<String, String>> getPassages() {
+		return lastQuery;
+	}
+
+	public List<ScoreWord> getTopNouns(){
+		if (topNouns == null) {
 			topNouns = RankNouns.findTopNouns(lastQuery);
 		}
 		return topNouns;
 	}
 	
-	public List<ScoreWord> findRerankedNouns() throws IOException{
+	public List<ScoreWord> getRerankedTopNouns() throws IOException{
 		if (topRerankedNouns == null) {
 			List<Pair<String, Double>> predictedCategories = Categorizer.getCategories(q);
 			List<Word[]> words = PosTagger.getInstance().tagString(q);
@@ -45,7 +45,7 @@ public class Minerva {
 			for (Word w : words.get(0)) {
 				qLemmas.add(w.lemma);
 			}
-			topRerankedNouns = Reranker.getInstance().rerank(findTopNouns(), qLemmas, predictedCategories);
+			topRerankedNouns = Reranker.getInstance().rerank(getTopNouns(), qLemmas, predictedCategories);
 		}
 		return topRerankedNouns;
 	}

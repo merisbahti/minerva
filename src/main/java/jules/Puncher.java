def3147560ef832a41e7d26753c8d434b3fdab9e
Dolck <tim.dolck@gmail.com>
2014-12-16 15:29:51 +0100

    Puncher workin

--- src/main/java/jules/Puncher.java
+++ src/main/java/jules/Puncher.java
@@ -19,21 +19,22 @@ public class Puncher {
 	public List<ScoreWord> punch(List<ScoreWord> topNouns, List<Pair<String, Double>> categories) {
 		List<ScoreWord> result = new ArrayList<ScoreWord>();
 		for (ScoreWord sw : topNouns) {
+			System.out.println(sw.lemma);
 			if (match(sw.neTypeTag, categories)) {
 				result.add(sw);
-				System.out.println(sw.lemma);
 			}
 		}
 		return result.size() > 100 ? result.subList(0, 100) : result;
 	}
 
 	private boolean match(String neTypeTag, List<Pair<String, Double>> categories) {
-		int noMatch = 0;
 		double limit = 0.001;
+		int noMatch = 0;
 		
 		for (Pair<String, Double> p : categories) {
+			System.out.println(p.fst + " " + p.snd);
 			if (p.snd > limit && matchMap.containsKey(p.fst)) {
-				String[] staggTags = matchMap.get(p.snd);
+				String[] staggTags = matchMap.get(p.fst);
 				for (String s : staggTags) {
 					System.out.println(p.fst + " " + s);
 					if (s.equalsIgnoreCase(neTypeTag))
@@ -43,8 +44,9 @@ public class Puncher {
 				noMatch ++;
 			}
 		}
-		if(noMatch == categories.size())
+		if(noMatch == categories.size()){
 			return true;
+		}
 		
 		return false;
 	}
